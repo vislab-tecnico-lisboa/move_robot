70fe6c8dfb7051bfe2d2cf3a8c05941637d9a820
ruipimentelfigueiredo <ruihortafigueiredo@gmail.com>
2016-07-08 18:45:56 +0100

    started addding saccadic suppresion stuff

--- move_robot/src/Gaze.cpp
+++ move_robot/src/Gaze.cpp
@@ -9,13 +9,84 @@ Gaze::Gaze(const std::string & name) :
     tf_listener(new tf::TransformListener(ros::Duration(40.0))),
     last_fixation_point(Eigen::Vector3d::Constant(std::numeric_limits<double>::max())),
     oculocephalic_group(new moveit::planning_interface::MoveGroup("oculocephalic")),
-    active(false)
+    active(false),
+    it(private_node_handle),
+    first_suppresion(true)
 {
     oculocephalic_joint_names=oculocephalic_group->getActiveJoints();
     oculocephalic_joint_values.resize(oculocephalic_joint_names.size());
     std::fill(oculocephalic_joint_values.begin(), oculocephalic_joint_values.end(), 0);
 
+    private_node_handle.param<std::string>("base_frame", base_frame_id, "base_frame");
+    private_node_handle.param("vel_threshold", vel_threshold, 0.1);
+
     fixation_point_goal_viz_pub = nh_.advertise<geometry_msgs::PointStamped>("fixation_point_goal_viz", 1);
+
+    ROS_ERROR("OLA");
+    // Saccadic
+    /*left_image_sub=boost::shared_ptr<message_filters::Subscriber<sensor_msgs::Image> > (new message_filters::Subscriber<sensor_msgs::Image>(private_node_handle, "left_camera_in", 1000));
+    right_image_sub=boost::shared_ptr<message_filters::Subscriber<sensor_msgs::Image> > (new message_filters::Subscriber<sensor_msgs::Image>(private_node_handle, "right_camera_in", 1000));
+    joint_state_sub=boost::shared_ptr<message_filters::Subscriber<sensor_msgs::JointState> > (new message_filters::Subscriber<sensor_msgs::JointState>(private_node_handle, "joint_states", 1000));
+
+    //TF's synchronized with the image
+    left_image_filter=boost::shared_ptr<tf::MessageFilter<sensor_msgs::Image> > (new tf::MessageFilter<sensor_msgs::Image>(*left_image_sub, *tf_listener, base_frame_id, 1000));
+    right_image_filter=boost::shared_ptr<tf::MessageFilter<sensor_msgs::Image> > (new tf::MessageFilter<sensor_msgs::Image>(*right_image_sub, *tf_listener, base_frame_id, 1000));
+
+    gaze_sync=boost::shared_ptr<message_filters::Synchronizer<MySyncPolicy> > (new message_filters::Synchronizer<MySyncPolicy>(MySyncPolicy(100),
+                                                                                                                               *left_image_filter,
+                                                                                                                               *right_image_filter,
+                                                                                                                               *joint_state_sub));
+
+    //gaze_sync->registerCallback(boost::bind(&Gaze::suppresion, this, _1, _2, _3));
+
+    left_image_suppression_pub=nh_.advertise<geometry_msgs::PointStamped>("left_camera_out", 1);
+    right_image_suppression_pub=nh_.advertise<geometry_msgs::PointStamped>("right_camera_out", 1);*/
+    ROS_ERROR("ADEUS");
+
+}
+
+void Gaze::suppresion(const sensor_msgs::Image::ConstPtr & left_image_msg,
+                      const sensor_msgs::Image::ConstPtr & right_image_msg,
+                      const sensor_msgs::JointState::ConstPtr & joint_state_msg)
+{
+    ROS_INFO("ENTREI");
+
+    // Get indices
+    if (first_suppresion)
+    {
+        for(int i=0; i<joint_state_msg->name.size();++i)
+        {
+            if(joint_state_msg->name[i]=="neck_pan_joint")
+            {
+                joints_to_indices["neck_pan_joint"]=i;
+            }
+            else if(joint_state_msg->name[i]=="neck_tilt_joint")
+            {
+                joints_to_indices["neck_tilt_joint"]=i;
+            }
+            else if(joint_state_msg->name[i]=="r_eye_joint")
+            {
+                joints_to_indices["r_eye_joint"]=i;
+            }
+        }
+
+        first_suppresion=false;
+    }
+
+    double n_pan=joint_state_msg->velocity[joints_to_indices["neck_pan_joint"]];
+    double n_tilt=joint_state_msg->velocity[joints_to_indices["neck_tilt_joint"]];
+    double eye=joint_state_msg->velocity[joints_to_indices["r_eye_joint"]];
+
+    double sum_vels=fabs(n_pan+n_tilt+eye); // e_talt = n_pan n_tilt e_pan e_tilt
+
+    if(sum_vels>vel_threshold)
+    {
+        ROS_INFO("ESTOU A SUPPRIMIR");
+        return;
+    }
+
+    left_image_suppression_pub.publish(left_image_msg);
+    right_image_suppression_pub.publish(right_image_msg);
 }
 
 void Gaze::publishFixationPointGoal()
--- move_robot/src/Gaze.h
+++ move_robot/src/Gaze.h
@@ -28,6 +28,9 @@
 
 #include <std_msgs/Float64.h>
 #include <geometry_msgs/PointStamped.h>
+#include <sensor_msgs/Image.h>
+#include <image_transport/image_transport.h>
+
 
 #include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
@@ -44,7 +47,35 @@
 class Gaze
 {
 
+    // Saccadic stuff
+    //Sync
+    image_transport::ImageTransport it;
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::JointState> MySyncPolicy;
+
+    boost::shared_ptr<message_filters::Subscriber<sensor_msgs::Image> > left_image_sub;
+    boost::shared_ptr<message_filters::Subscriber<sensor_msgs::Image> > right_image_sub;
+
+    boost::shared_ptr<message_filters::Synchronizer<MySyncPolicy> > gaze_sync;
+
+    //Message filter to sync tfs with image
+    boost::shared_ptr<tf::MessageFilter<sensor_msgs::Image> > left_image_filter;
+    boost::shared_ptr<tf::MessageFilter<sensor_msgs::Image> > right_image_filter;
+
+    void suppresion(const sensor_msgs::Image::ConstPtr & left_image_msg,
+                    const sensor_msgs::Image::ConstPtr & right_image_msg,
+                    const sensor_msgs::JointState::ConstPtr & joint_state_msg);
+
+    bool first_suppresion;
+    ros::Publisher left_image_suppression_pub;
+    ros::Publisher right_image_suppression_pub;
+
+    std::string base_frame_id;
+    double vel_threshold;
+
+    std::map<std::string, int> joints_to_indices;
+
 protected:
+    boost::shared_ptr<message_filters::Subscriber<sensor_msgs::JointState> > joint_state_sub;
 
     bool active;
     double y_offset;
@@ -77,9 +108,7 @@ protected:
     move_robot_msgs::GazeFeedback feedback_;
     move_robot_msgs::GazeResult result_;
 
-
     boost::shared_ptr<message_filters::Subscriber<geometry_msgs::PointStamped> > fixation_point_sub;
-    boost::shared_ptr<message_filters::Subscriber<sensor_msgs::JointState> > joint_state_sub;
 
     // Publishers
     ros::Publisher neck_pan_pub;
@@ -88,6 +117,9 @@ protected:
     ros::Publisher l_eye_pub;
     ros::Publisher r_eye_pub;
     ros::Publisher fixation_point_goal_viz_pub;
+
+
+
     void publishFixationPointGoal();
 
     virtual bool moveHome() = 0;
